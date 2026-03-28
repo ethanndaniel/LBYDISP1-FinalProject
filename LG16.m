f8a6c3d7a37a83042cb346cd7d9a33916fa412a0
ethanndaniel <ethan_daniel_magbatoc@dlsu.edu.ph>
2026-03-28 20:58:08 +0800

    Add files via upload

---
+++ LG16.m
@@ -0,0 +1,37 @@
+%% LBYDISP1 Final Project
+
+
+%% 1. Import Audio
+
+% make sure to download .wav file
+[x, Fs] = audioread('NewsReportSignal-1.wav');
+t = (0:length(x)-1)/Fs;
+
+% plot figure
+figure;
+plot(t, x);
+title('Noisy Signal (Time Domain)');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+% play NewsReportSignal-1.wav
+sound(x, Fs);
+
+
+
+
+%% 2. Apply Filters
+
+
+% A. Adaptive Filter
+
+
+% B. FIR Filter
+
+
+
+
+
+%% 3. Comparison of Results
+
+
